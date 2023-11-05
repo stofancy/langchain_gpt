aac1f604b8f545cf6d1e760caa88085aeaa2e3ce
Kaihang An <ztmdsbt@hotmail.com>
2023-11-05 19:52:12 +0000

    clean the project.

--- intro/memory.py
+++
@@ -1,9 +0,0 @@
-"""_summary_Memory：就是提供对话的上下文存储，可以使用Langchain的ConversationChain，在LLM交互中记录交互的历史状态，并基于历史状态修正模型预测。"""
-from langchain import OpenAI, ConversationChain
-
-llm = OpenAI(temperature=0)
-conversation = ConversationChain(llm=llm, verbose=True)
-print("input text: conversion")
-
-conversation.predict(input="Nice to meet you.")
-conversation.predict(input="I'm fine. this is my first AI project.")
