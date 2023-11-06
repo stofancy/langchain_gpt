90f078b74ebd11fd9392daf3b775b84ace5cd0e7
Kaihang An <ztmdsbt@hotmail.com>
2023-11-06 18:45:27 +0000

    fine tuning the prompt.

--- hackathon.py
+++ hackathon.py
@@ -13,11 +13,12 @@ st.set_page_config(page_title='hackathon')
 st.header('Hackathon')
 
 uploaded_file = st.file_uploader(
-    'upload meeting transcript file', type=['txt', 'md'])
+    'upload meeting transcript file', type=['txt', 'md', 'vtt'])
 
 
 def refine_chain(target_len, docs, openaichat):
     prompt_template = """Act as a professional technical meeting minutes writer. 
+    meeting was record as ".vtt" file stands for "Web Video Text Tracks.", firstly, you need to parse it, extract the participants and their speeches.
     Tone: formal
     Format: Technical meeting summary
     Length:  200 ~ 300
@@ -61,7 +62,7 @@ def refine_chain(target_len, docs, openaichat):
 
 
 def stuff_chain(target_len, docs, openaichat):
-    prompt_template = f"""Act as a professional technical meeting minutes writer. 
+    prompt_template = f"""Act as a professional technical meeting minutes writer.
     Tone: formal
     Format: Technical meeting summary
     Length:  200 ~ 300
@@ -70,12 +71,18 @@ def stuff_chain(target_len, docs, openaichat):
     - highlight the agreements
     - Use bullet points if needed
     {{text}}
-    For the tasks are mentioned in the meeting, which formated as Story-xxx, they are task id in the Jira website, the url of related task should be like `https://hackathon.jira.com/browser/Story-xxx`, url started with `https://hackathon.jira.com/browser/` and ended with the actual task id.
+    Above is the metting record,each sentence is recorded in to 2 rows like below:
+    00:25:29.570 --> 00:25:29.870
+    <v first_name, last_name>sentence</v>
+    The first row is the time period while participant was speaking, the second row is the participant name and their sentence of they was speaking. if the participant name looks like a GUID or UUID, treat it as "Lucia Qiao".
+    Firstly, you need to parse the content, extract the participants and their speeches. And then, based on the parsed content,
     CONCISE SUMMARY IN ENGLISH, this is the first step.
     And then we have to refine the summary to final summary in English within {target_len} words: following the format:\n"
-    Participants: <participants>
-    Discussed: <Discussed-items>
+    Participants: <participants>\n
+    Discussed: <Discussed-items>\n
     Follow-up actions: <a-list-of-follow-up-actions-with-owner-names-and-the-related-task-url>
+    -------------------------------------------
+    For the tasks are mentioned in the meeting, which formated as Story-xxx, they are task id in the Jira website, the url of related task should be like `https://hackathon.jira.com/browser/Story-xxx`, url started with `https://hackathon.jira.com/browser/` and ended with the actual task id.
     If the context isn't useful, return the original summary. Highlight agreements and follow-up actions and owners.
     """
     prompt = PromptTemplate.from_template(prompt_template)
@@ -105,7 +112,7 @@ if uploaded_file is not None:
     transcript_str = transcript.read()
     # st.write(f"file content:\n {transcript_str}")
 
-    target_len = 100
+    target_len = 200
     chunk_size = 3000
     chunk_overlap = 200
 
@@ -120,13 +127,12 @@ if uploaded_file is not None:
     # st.write(len(texts))
     # st.write(len(docs))
 
-    openaichat = ChatOpenAI(temperature=0, model="gpt-4")
-    resp = ""
-    if len(docs) > 1:
-        st.write("refine_chain")
-        resp = refine_chain(target_len, docs, openaichat)
-        st.write(resp["intermediate_steps"])
-    else:
-        resp = stuff_chain(target_len, docs, openaichat)
+    openaichat = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
+    # resp = ""
+    # if len(docs) > 1:
+    resp = stuff_chain(target_len, docs, openaichat)
+    # st.write(resp["intermediate_steps"])
+    # else:
+    #     resp = stuff_chain(target_len, docs, openaichat)
     st.write("========== MEETING MINUTES ==========")
     st.write(resp["output_text"])
